9c12bfbc41cc117e8bf569790e148fdd15f91ac6
Poojitha-519 <athmakurupoojitha@gmail.com>
2024-09-19 14:12:58 +0530

    fruit selier.py

---
+++ fruit selier.py
@@ -0,0 +1,63 @@
+'''
+ A fruit seller buys a dozen of banana at Rs.X. He sells 1 banana at Rs.Y. Write a program to determine the profit or loss in Rs. for the fruitseller.
+Input format:
+Input consists of 2 floating point numbers
+The first input corresponds to the total cost(X)
+The second input corresponds to the sold cost(Y)
+Output format:
+Print "Profit or Loss" with Rupees.
+Sample Input:
+60
+4
+Sample Output:
+Loss : Rs.12.00
+
+5) 
+Write a program to determine the fee amount to be collected from a student. 
+Refer the table below for fee details.
+  Student Type  
+  Student Type denoted as  
+  Fee Details  
+Merit Seat Day Scholar
+MSDS
+
+Tuition fee + Bus fee
+
+
+
+Merit Seat Hosteller
+MSH
+Tuition fee + Hostel fee
+
+Management Seat Day Scholar
+
+
+
+MGSDS
+
+150% of Tuition fee + Bus fee
+
+
+
+Management Seat Hosteller
+MGSH
+
+150% of Tuition fee + Hostel fee
+
+
+
+
+Input format:
+The first input corresponds to the student type
+The second input corresponds to the tuition fee
+The third input corresponds to the bus fee or hostel fees
+Output format:
+Print the total fee amount of the corresponding student with 2 decimal places.
+Refer below sample output for formatting
+Sample Input:
+MSH
+40000
+50000
+Sample Output:
+90000.00
+'''
