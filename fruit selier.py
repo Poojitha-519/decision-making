4395968c754bb6098ae2e8237b2bfeaaf061efdb
Poojitha-519 <athmakurupoojitha@gmail.com>
2024-09-23 21:31:59 +0530

    Update fruit selier.py

--- fruit selier.py
+++ fruit selier.py
@@ -61,3 +61,13 @@ MSH
 Sample Output:
 90000.00
 '''
+
+X = float(input()) 
+Y = float(input())  
+total_selling_price = Y * 12
+profit_or_loss = total_selling_price - X
+if profit_or_loss > 0:
+    print(f"Profit : Rs.{profit_or_loss:.2f}")
+else:
+    print(f"Loss : Rs.{abs(profit_or_loss):.2f}")
+
